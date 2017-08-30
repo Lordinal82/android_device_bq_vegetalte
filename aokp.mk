19305c4242658943c74e4f199b3555332e656518
Lordinal82 <loren.canosan@gmail.com>
2017-08-30 11:32:38 +0200

    vegetalte: switched LineageOS makefiles with AOKP ones
    Change-Id: I975b7463b662c8aa713f400cfd5c466d41382314

--- aokp.mk
+++ aokp.mk
@@ -3,13 +3,13 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/bq/vegetalte/full_vegetalte.mk)
 
 PRODUCT_RELEASE_NAME := Aquaris E5
-PRODUCT_NAME := lineage_vegetalte
+PRODUCT_NAME := aokp_vegetalte
 
 # Set product device & name
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -20,3 +20,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
    PRIVATE_BUILD_DESC="vegetalte-user 6.0.1 MMB29M 4.0.0.04_20161125-1352 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-bq
+
